18aa770837587e03146e97351ada03b6195107ea
Quyunshuo <quyunshuo@qq.com>
2023-09-09 17:53:02 +0800

    feat(Information): 👾添加学习页面的文章列表

--- Information/src/main/java/com/quyunshuo/compose/information/ui/components/ArticleItem.kt
+++ Information/src/main/java/com/quyunshuo/compose/information/ui/components/ArticleItem.kt
@@ -1,15 +1,58 @@
 package com.quyunshuo.compose.information.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.quyunshuo.compose.information.model.ArticleEntity
 
 @Composable
 fun ArticleItem(article: ArticleEntity) {
+    Column {
+        Text(
+            text = article.title,
+            color = Color(0xFF333333),
+            fontSize = 16.sp,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.padding(8.dp)
+        )
+        Row {
+            Text(
+                text = "来源:${article.source}", color = Color(0xFF999999),
+                fontSize = 10.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = article.timestamp,
+                color = Color(0xFF999999),
+                fontSize = 10.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
 
+        Spacer(Modifier.height(8.dp))
+
+        Divider()
+    }
 }
 
-@Preview
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 fun ArticleItemPreview() {
     val articleEntity = ArticleEntity(
