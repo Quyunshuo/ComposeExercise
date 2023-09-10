0c2055e9b8d09763e856249383eb1607fdef8c59
Quyunshuo <quyunshuo@qq.com>
2023-09-10 23:44:15 +0800

    feat(Information): 🍉任务界面添加顶部标题，优化状态栏高度

--- Information/src/main/java/com/quyunshuo/compose/information/ui/components/TopAppBar.kt
+++ Information/src/main/java/com/quyunshuo/compose/information/ui/components/TopAppBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -42,8 +43,8 @@ fun TopAppBar(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
                     )
                 )
             )
-            .height(appBarHeight + 20.dp)
-            .padding(top = 20.dp)
+            .statusBarsPadding()
+            .height(appBarHeight)
             .then(modifier),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
--- Information/src/main/java/com/quyunshuo/compose/information/ui/screens/TaskScreen.kt
+++ Information/src/main/java/com/quyunshuo/compose/information/ui/screens/TaskScreen.kt
@@ -1,22 +1,55 @@
 package com.quyunshuo.compose.information.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.quyunshuo.compose.information.ui.components.appBarHeight
 
 @Composable
 fun TaskScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xB54CAF50)),
-        contentAlignment = Alignment.Center
+            .background(
+                Brush.verticalGradient(
+                    listOf(Color(0xFF149EE7), Color.White)
+                )
+            )
     ) {
-        Text(text = "Task")
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .statusBarsPadding()
+                .height(appBarHeight)
+        ) {
+            Text(
+                text = "学习任务",
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun TaskScreenPreview() {
+    TaskScreen()
 }
